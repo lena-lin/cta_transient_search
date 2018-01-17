ce6e5789b6e38913e47de9d61b83e2eb7327f808
llinhoff <lena.linhoff@tu-dortmund.de>
2018-01-17 12:17:35 +0100

    fix false positive

--- evaluation.py
+++ evaluation.py
@@ -9,7 +9,6 @@ from IPython import embed
 @click.argument('input_simulation', type=click.Path(file_okay=True, dir_okay=False))
 @click.argument('input_alert', type=click.Path(file_okay=True, dir_okay=False))
 @click.argument('output_file', type=click.Path(file_okay=True, dir_okay=False))
-
 def main(
     input_simulation,
     input_alert,
@@ -27,13 +26,13 @@ def main(
         diff_flarestart = np.asarray(table_simulation['start_flare'] - table_alert['first_trigger'])
         tp = len(diff_flarestart[abs(diff_flarestart) <= 2])
         fn = len(diff_flarestart[np.isnan(diff_flarestart)])
-        fp = len(diff_flarestart[abs(diff_flarestart) > 2]) - fn
+        fp = len(diff_flarestart[abs(diff_flarestart) > 2])
         tn = 0
 
         print('TP-Rate: {} \n FN-Rate: {} \n FP-Rate: {}'.format(tp/num_cubes, fn/num_cubes, fp/num_cubes))
         print('TP: {} \n FN: {} \n FP: {} \n TN: {}'.format(tp, fn, fp, tn))
         f = open('build/{}'.format(output_file), 'w')
-        f.writelines('TP-Rate: {} \n FN-Rate: {} \n FP-Rate: {}'.format(tp/num_cubes, fn/num_cubes, fp/num_cubes))
+        f.writelines('Number of simulated transients: {} \n TP: {} \n FN: {} \n FP: {} \n TN: {}'.format(num_cubes, tp, fn, fp, tn))
         f.close()
 
 
