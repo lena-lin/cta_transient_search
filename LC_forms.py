c901a196e52c39559dbc2a3ee4bb473fc51c242f
llinhoff <lena.linhoff@tu-dortmund.de>
2018-02-23 12:28:04 +0100

    fix typos, style

--- LC_forms.py
+++ LC_forms.py
@@ -15,11 +15,13 @@ Templates = y-data of fast transient describes by above form
 
 import numpy as np
 
-def Gauss(x, a, x0, sigma,b):
+
+def Gauss(x, a, x0, sigma, b):
     return a*np.exp(-(x-x0)**2/(2*sigma**2))+b
 
+
 @np.vectorize
-def Step_exp(t,tp,Fp,b ,alpha):
+def Step_exp(t, tp, Fp, b, alpha):
     if t < tp:
         return float(0)  # uniform data types
     if t >= tp:
@@ -27,44 +29,48 @@ def Step_exp(t,tp,Fp,b ,alpha):
 
 
 ### Fitting parameters averaged over 5-17 transients
-Mean_params_Simple = np.array([ 0.626635975768735 ,12.860830849499434, 4.6052849965482245, 0.1331112404132909 ])
-Mean_params_Small = np.array([0.8957302067729599 ,11.612091524250664, 0.9982224038570281 ,0.11782671354588838 ])
-Mean_params_Exp = np.array([ 2.86633333,  0.8355    , -1.43666667,  1.41788333])
+Mean_params_Simple = np.array([0.626635975768735, 12.860830849499434, 4.6052849965482245, 0.1331112404132909])
+Mean_params_Small = np.array([0.8957302067729599, 11.612091524250664, 0.9982224038570281, 0.11782671354588838])
+Mean_params_Exp = np.array([2.86633333, 0.8355, -1.43666667,  1.41788333])
+
 
 ### Broad round gaussian
-def Simple_Gaussian(Num_slices,noise_percentage):
-    Timestamp = np.linspace(0,Num_slices,Num_slices)
-    x_Gauss  = range(int(4*Mean_params_Simple[1]))-Mean_params_Simple[1]
+def broad_gaussian(Num_slices, noise_percentage):
+    Timestamp = np.linspace(0, Num_slices, Num_slices)
+    x_Gauss = range(int(4*Mean_params_Simple[1])) - Mean_params_Simple[1]
     # intepolation to guarantee correct form
-    x_p = np.linspace(Timestamp[0],Num_slices,len(x_Gauss))
-    y_p = (Gauss(x_Gauss,Mean_params_Simple[0], Mean_params_Simple[1], Mean_params_Simple[2], Mean_params_Simple[3])-Mean_params_Simple[3])/Mean_params_Simple[0] ## Normalized 1
-    y = np.interp(Timestamp,x_p,y_p)
+    x_p = np.linspace(Timestamp[0], Num_slices, len(x_Gauss))
+    y_p = (Gauss(x_Gauss, Mean_params_Simple[0], Mean_params_Simple[1], Mean_params_Simple[2], Mean_params_Simple[3]) - Mean_params_Simple[3]) / Mean_params_Simple[0] ## Normalized 1
+    y = np.interp(Timestamp, x_p, y_p)
     # add noise
-    noise = np.random.normal(0,noise_percentage/100.0, y.shape)
+    noise = np.random.normal(0, noise_percentage/100.0, y.shape)
     y = y + noise
-    return y
+    return abs(y)
+
 
 ### small pointed gaussian
-def Small_Gaussian(Num_slices,noise_percentage):
-    Timestamp = np.linspace(0,Num_slices,Num_slices)
-    x_Gauss  = range(int(4*Mean_params_Small[1]))-Mean_params_Small[1]
-    x_p = np.linspace(Timestamp[0],Num_slices,len(x_Gauss))
-    y_p = (Gauss(x_Gauss,Mean_params_Small[0], Mean_params_Small[1], Mean_params_Small[2], Mean_params_Small[3])-Mean_params_Small[3])/Mean_params_Small[0]
-    y = np.interp(Timestamp,x_p,y_p)
+def narrow_gaussian(Num_slices, noise_percentage):
+    Timestamp = np.linspace(0, Num_slices, Num_slices)
+    x_Gauss = range(int(4*Mean_params_Small[1]))-Mean_params_Small[1]
+    x_p = np.linspace(Timestamp[0], Num_slices, len(x_Gauss))
+    y_p = (Gauss(x_Gauss, Mean_params_Small[0], Mean_params_Small[1], Mean_params_Small[2], Mean_params_Small[3])-Mean_params_Small[3])/Mean_params_Small[0]
+    y = np.interp(Timestamp, x_p, y_p)
     noise = np.random.normal(0, noise_percentage/100.0, y.shape)
     y = y + noise
-    return y
+    return abs(y)
+
 
 ### exponential decay after delta Peak
-def Exponential(Num_slices, noise_percentage):
-    Timestamp = np.linspace(0,Num_slices,Num_slices)
-    x_Exp = np.linspace(-30,170)
-    x_p = np.linspace(Timestamp[0],Num_slices,len(x_Exp))
-    y_p = Step_exp(x_Exp,Mean_params_Exp[0],Mean_params_Exp[1], Mean_params_Exp[2],Mean_params_Exp[3])/ Step_exp(x_Exp,Mean_params_Exp[0], Mean_params_Exp[1], Mean_params_Exp[2], Mean_params_Exp[3]).max()
-    y = np.interp(Timestamp,x_p,y_p)
+def deltapeak_exponential(Num_slices, noise_percentage):
+    Timestamp = np.linspace(0, Num_slices, Num_slices)
+    x_Exp = np.linspace(-30, 170)
+    x_p = np.linspace(Timestamp[0], Num_slices, len(x_Exp))
+    y_p = Step_exp(x_Exp, Mean_params_Exp[0], Mean_params_Exp[1], Mean_params_Exp[2], Mean_params_Exp[3]) / Step_exp(x_Exp, Mean_params_Exp[0], Mean_params_Exp[1], Mean_params_Exp[2], Mean_params_Exp[3]).max()
+    y = np.interp(Timestamp, x_p, y_p)
     noise = np.random.normal(0, noise_percentage/100.0, y.shape)
     y = y + noise
-    return y
+    return abs(y)
+
 
 '''''''''''''''''''''''''''
 Calling up the functions
