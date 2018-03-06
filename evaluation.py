c0c7643086cecddb1e84e44202932ba69bbd5145
llinhoff <lena.linhoff@tu-dortmund.de>
2018-03-06 16:40:33 +0100

    change structure, add def accuracy

--- evaluation.py
+++ evaluation.py
@@ -5,19 +5,7 @@ from astropy.table import Table
 from IPython import embed
 
 
-@click.command()
-@click.argument('input_simulation', type=click.Path(file_okay=True, dir_okay=False))
-@click.argument('input_alert', type=click.Path(file_okay=True, dir_okay=False))
-@click.argument('output_file', type=click.Path(file_okay=True, dir_okay=False))
-def main(
-    input_simulation,
-    input_alert,
-    output_file
-):
-
-    table_simulation = Table.read(input_simulation, path='data')
-    table_alert = Table.read(input_alert, path='data')
-
+def accuracy(table_simulation, table_alert):
     if len(table_simulation) != len(table_alert):
         print('Input tables do not have the same length!')
 
@@ -28,6 +16,22 @@ def main(
         fn = len(diff_flarestart[np.isnan(diff_flarestart)])
         fp = len(diff_flarestart[abs(diff_flarestart) > 2])
         tn = 0
+
+        return tp, fp, tn, fn, num_cubes
+
+
+@click.command()
+@click.argument('input_simulation', type=click.Path(file_okay=True, dir_okay=False))
+@click.argument('input_alert', type=click.Path(file_okay=True, dir_okay=False))
+@click.argument('output_file', type=click.Path(file_okay=True, dir_okay=False))
+def main(
+    input_simulation,
+    input_alert,
+    output_file
+):
+        table_simulation = Table.read(input_simulation, path='data')
+        table_alert = Table.read(input_alert, path='data')
+        tp, fp, tn, fn, num_cubes = accuracy(table_simulation, table_alert)
 
         print('TP-Rate: {} \n FN-Rate: {} \n FP-Rate: {}'.format(tp/num_cubes, fn/num_cubes, fp/num_cubes))
         print('TP: {} \n FN: {} \n FP: {} \n TN: {}'.format(tp, fn, fp, tn))
