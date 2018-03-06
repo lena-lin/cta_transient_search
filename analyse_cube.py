a2fdb3c1b21d29b8490c7c59941d12cd1bc3fe37
Lena Linhoff <lena.linhoff@googlemail.com>
2018-03-06 10:46:57 +0100

    split denoised table into cube + trans_factor

--- analyse_cube.py
+++ analyse_cube.py
@@ -81,7 +81,6 @@ def main(
 
     denoised_table = Table()
     denoised_table['cube_smoothed'] = list_cubes_denoised
-    denoised_table['trans_factor'] = denoised_table['cube_smoothed'].max(axis=2).max(axis=2)
 
     denoised_table.meta = cube_raw_table.meta
     denoised_table.meta['n_bg_slices'] = n_bg_slices
@@ -89,6 +88,8 @@ def main(
 
     denoised_table.write('{}/n{}_s{}_t{}_denoised.hdf5'.format(output_path, n_transient, num_slices, transient_template_index), path='data', overwrite=True)
 
+    trans_factor_table = Table({'trans_factor' : denoised_table['cube_smoothed'].max(axis=2).max(axis=2)})
+    trans_factor_table.write('{}/n{}_s{}_t{}_trans_factor.hdf5'.format(output_path, n_transient, num_slices, transient_template_index), path='data', overwrite=True)
 
 if __name__ == '__main__':
     main()
