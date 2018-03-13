076171d473fbb9b542312b46eab203e75f441999
llinhoff <lena.linhoff@tu-dortmund.de>
2018-03-13 16:43:43 +0100

    add meta to trigger table

--- analyse_cube.py
+++ analyse_cube.py
@@ -89,6 +89,7 @@ def main(
     denoised_table.write('{}/n{}_s{}_t{}_denoised.hdf5'.format(output_path, n_transient, num_slices, transient_template_index), path='data', overwrite=True)
 
     trans_factor_table = Table({'trans_factor': denoised_table['cube_smoothed'].max(axis=2).max(axis=2)})
+    trans_factor_table.meta = cube_raw_table.meta
     trans_factor_table.write('{}/n{}_s{}_t{}_trigger.hdf5'.format(output_path, n_transient, num_slices, transient_template_index), path='data', overwrite=True)
 
 
