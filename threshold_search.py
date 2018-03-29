bf1917283f374691d3848e51a30681954d64a09d
llinhoff <lena.linhoff@tu-dortmund.de>
2018-03-29 14:49:39 +0200

    change path for waveletanalysis

--- threshold_search.py
+++ threshold_search.py
@@ -5,28 +5,28 @@ from click.testing import CliRunner
 
 
 def run_threshold_transients():
-    files = glob.glob('build/n200_s60_t*_trigger.hdf5')
+    files = glob.glob('build/wavelet_studies/n200_s60_t*_trigger.hdf5')
     print(files)
     runner = CliRunner()
 
     for template_trigger in tqdm(files):
         for th in range(1, 26):
-            result = runner.invoke(transient_alert.main, [template_trigger, '--output_path', 'build/threshold_studies/grid_search', '-t', str(th)])
+            result = runner.invoke(transient_alert.main, [template_trigger, '--output_path', 'build/wavelet_studies/grid_search', '-t', str(th)])
             if result.exit_code != 0:
                 print(result.exit_code)
 
 
 def run_threshold_background():
-    background_trigger = 'build/background_studies/nNone_s60_tNone_trigger.hdf5'
+    background_trigger = 'build/wavelet_studies/nNone_s60_tNone_trigger.hdf5'
     runner = CliRunner()
     for th in tqdm(range(1, 26)):
-        result = runner.invoke(transient_alert.main, [background_trigger, '--output_path', 'build/background_studies/grid_search', '-t', str(th)])
+        result = runner.invoke(transient_alert.main, [background_trigger, '--output_path', 'build/wavelet_studies/grid_search', '-t', str(th)])
         if result.exit_code != 0:
             print(result.exit_code)
 
 def main():
     run_threshold_transients()
-    ##run_threshold_background()
+    run_threshold_background()
 
 if __name__ == '__main__':
     main()
