5c8438885eb1d8bc0da1fcc323a356f04cc57b5c
Lena Linhoff <lena.linhoff@googlemail.com>
2018-08-14 13:11:56 +0200

    filename fit make

--- simulate_cube.py
+++ simulate_cube.py
@@ -277,8 +277,8 @@ def main(
     trans_table.meta['time_per_slice'] = time_per_slice
     trans_table.meta['bins'] = bins_
 
-    cube_table.write('{}/n{}_s{}_t{}_cube.hdf5'.format(output_path, n_transient, 3*num_slices, transient_template_index), path='data', overwrite=True)
-    trans_table.write('{}/n{}_s{}_t{}_trans.hdf5'.format(output_path, n_transient, 3*num_slices, transient_template_index), path='data', overwrite=True)
+    cube_table.write('{}/n{}_s{}_t{}_cube.hdf5'.format(output_path, n_transient, 3*num_slices, transient_template_filename), path='data', overwrite=True)
+    trans_table.write('{}/n{}_s{}_t{}_trans.hdf5'.format(output_path, n_transient, 3*num_slices, transient_template_filename), path='data', overwrite=True)
 
 
 if __name__ == '__main__':
