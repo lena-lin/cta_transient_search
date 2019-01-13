5560020138f956f1bb1b51797339e1b574eea52d
Lena Linhoff <lena.linhoff@googlemail.com>
2019-01-13 20:15:31 +0100

    add ebl

--- cta_transient_search/simulation/spectrum.py
+++ cta_transient_search/simulation/spectrum.py
@@ -1,9 +1,7 @@
 import numpy as np
 import astropy.units as u
-from scipy.integrate import quad
+from scipy.integrate import trapz
 from ebltable.tau_from_model import OptDepth
-
-#tau = OptDepth.readmodel(model = 'dominguez')
 
 
 def random_power(e_min, e_max, N):
@@ -13,9 +11,10 @@ def random_power(e_min, e_max, N):
     index = 2.48
     u = np.random.uniform(0, 1, N)
     # inverse transform sampling
-    return ((e_max**(1-index) - e_min**(1-index)) * u + e_min**(1-index))**(1/(1-index))
+    return ((e_max**(1 - index) - e_min**(1 - index)) * u + e_min**(1-index))**(1 / (1 - index))
 
 
+@u.quantity_input(T = u.s, e_min = u.TeV, e_max = u.TeV,simulation_area = u.m**2)
 def number_particles_crab(T, e_min, e_max, simulation_area):
     '''
     Returns the number of particles arriving from a pointlike source with known power law distribution
@@ -34,17 +33,15 @@ def number_particles_crab(T, e_min, e_max, simulation_area):
     # C = 2.83e-11 / (u.cm**2 * u.s * u.TeV)
     # E_0 = 1 * u.TeV
 
-    # Spectrum used in ctools, energy in MeV!!!!!!!!
+    # Spectrum used in ctools
     index = 2.48
-    C = 5.7e-16 / (u.cm**2 * u.s * u.MeV)
-    E_0 = 0.3e6 * u.MeV
-    e_min = e_min.to(u.MeV)
-    e_max = e_max.to(u.MeV)
+    C = u.Quantity(5.7e-6,  1/ (u.m**2 * u.s * u.TeV))
+    E_0 = u.Quantity(0.3, u.TeV)
 
-    return int((simulation_area.to(u.cm**2)*C*T*E_0**(index)/(1-index))*((e_max)**(1-index) - (e_min)**(1-index)))
+    return int((simulation_area.to(u.cm**2) * C * T * E_0**(index) / (1 - index)) * ((e_max)**(1 - index) - (e_min)**(1 - index)))
 
-
-def number_particles_trans(T, e_min, e_max, simulation_area, Redshift):
+@u.quantity_input(T = u.s, e_min = u.TeV, e_max = u.TeV,simulation_area = u.m**2)
+def number_particles_trans(T, e_min, e_max, simulation_area, z):
     '''
     Returns the number of particles arriving from a pointlike source with known power law distribution and EBL attenuation
     Parameters:
@@ -57,26 +54,16 @@ def number_particles_trans(T, e_min, e_max, simulation_area, Redshift):
     z: Redshift
     '''
     index = 2.48
-    C = 5.7e-16 / (u.cm**2 * u.s * u.MeV)
-    E_0 = 0.3e6 * u.MeV
-    e_min = e_min.to(u.MeV)
-    e_max = e_max.to(u.MeV)
+    C = u.Quantity(5.7e-6,  1 / (u.m**2 * u.s * u.TeV))
+    E_0 = u.Quantity(0.3, u.TeV)
 
-    simulated_area = simulation_area.to(u.cm**2)
-    sim_area = simulated_area.value
-    mean_e = (emax+emin)/2
-    z = Redshift[0]
+    tau = OptDepth.readmodel(model = 'dominguez')
 
-    '''
-    integrate with quad to get more differenced EBL attenuation:
-    integrate:  E**(-index)*np.exp(-1.0*tau.opt_depth(z,E)) from E_min to E_max
-    '''
     def spectrum_crab_ebl(z):
-         return lambda E : (C * (E/E_0) ** (-index) * np.exp(-1.0*tau.opt_depth(z,E.value))).value
+        return lambda E : C * (E / E_0) ** (-index) * np.exp(-1.0 * tau.opt_depth(z, E.to_value(u.TeV)))
 
-    I = quad(spectrum_crab_ebl(z),emin,emax)[0]
+    E = np.linspace(e_min, e_max, 20000)
 
-    print('Simulated EBL attenuation with z= ',z,
-    '\n N without EBL =',int((sim_area*C*T.value*E_0**(index)/(1-index))*((emax)**(1-index) - (emin)**(1-index))),
-    '\n N is now = ',int((sim_area*C*T.value*E_0**(index))*I))
-    return int((sim_area*C*T.value*E_0**(index))*I)
+    I = trapz(spectrum_crab_ebl(z)(E), E)
+
+    return int(simulation_area * T * I)
