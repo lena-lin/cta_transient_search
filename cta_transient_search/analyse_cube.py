38c2f1792f51517674afd14f9b5691786abc962e
llinhoff <lena.linhoff@tu-dortmund.de>
2019-02-04 11:45:48 +0100

    add gradient

--- cta_transient_search/analyse_cube.py
+++ cta_transient_search/analyse_cube.py
@@ -13,22 +13,39 @@ def wavelet_denoising_cube(
     bins,
     n_wavelet_slices,
 ):
-        cube_without_steady_source = remove_steady_background(cube_raw, n_bg_slices, gap)
-        cube_denoised = [np.zeros([80, 80])]*(gap+n_bg_slices+n_wavelet_slices)
+    cube_without_steady_source = remove_steady_background(cube_raw, n_bg_slices, gap)
+    cube_denoised = [np.zeros([80, 80])]*(gap+n_bg_slices+n_wavelet_slices)
 
-        for i in range(n_wavelet_slices, len(cube_without_steady_source)):
+    for i in range(n_wavelet_slices, len(cube_without_steady_source)):
 
-            coeffs = pywt.swtn(
-                            data=cube_without_steady_source[i-n_wavelet_slices:i],
-                            wavelet='bior1.3',
-                            level=2,
-                            start_level=0
-                        )
-            ct = thresholding_3d(coeffs, k=30)
-            slice_denoised = pywt.iswtn(coeffs=ct, wavelet='bior1.3')[-1]
-            cube_denoised.append(slice_denoised)
+        coeffs = pywt.swtn(
+                        data=cube_without_steady_source[i-n_wavelet_slices:i],
+                        wavelet='bior1.3',
+                        level=2,
+                        start_level=0
+                    )
+        ct = thresholding_3d(coeffs, k=30)
+        slice_denoised = pywt.iswtn(coeffs=ct, wavelet='bior1.3')[-1]
+        cube_denoised.append(slice_denoised)
 
-        return np.asarray(cube_denoised)
+    return np.asarray(cube_denoised)
+
+
+def gradient_benchmark(
+    cube_raw
+):
+    bins = cube_raw.shape(1)
+    grad = np.diff(cube_raw, axis=0)
+    return np.vstack((np.zeros((1, bins, bins)), grad))
+
+
+def max_pixel_position(
+    cube_denoised,
+):
+    list_pos = []
+    for s in cube_denoised:
+        list_pos.append(np.unravel_index(np.argmax(s, axis=None), s.shape))
+    return list_pos
 
 
 @click.command()
@@ -78,11 +95,13 @@ def main(
     except:
         transient_template_index = None
 
-    bins = cube_raw_table.meta['bins']
-
     list_cubes_denoised = []
+    list_trigger_position = []
     for cube in tqdm(cube_raw_table['cube']):
-        cube_denoised = wavelet_denoising_cube(cube, n_bg_slices, gap, bins, n_wavelet_slices)
+        cube_denoised = gradient_benchmark(cube)
+        pos_trigger_pixel = max_pixel_position(cube_denoised)
+
+        list_trigger_position.append(pos_trigger_pixel)
         list_cubes_denoised.append(cube_denoised)
 
     denoised_table = Table()
@@ -95,17 +114,8 @@ def main(
 
     denoised_table.write('{}/n{}_s{}_t{}_denoised.hdf5'.format(output_path, n_transient, num_slices, transient_template_index), path='data', overwrite=True)
 
-# Not needed anymore after changes in transient_alert.py
-    list_position_c = []
-
-    for c in list_cubes_denoised:
-        list_pos = []
-        for s in c:
-            list_pos.append(np.unravel_index(np.argmax(s, axis=None), s.shape))
-        list_position_c.append(list_pos)
-
     trans_factor_table = Table({'trans_factor': denoised_table['cube_smoothed'].max(axis=2).max(axis=2),
-                                'trigger_pos': list_position_c})
+                                'trigger_pos': list_trigger_position})
     trans_factor_table.meta = denoised_table.meta
     trans_factor_table.write('{}/n{}_s{}_t{}_trigger.hdf5'.format(output_path, n_transient, num_slices, transient_template_index), path='data', overwrite=True)
 
