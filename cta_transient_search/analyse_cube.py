f2f5ccf654c5354ea8c5c7be1d0e1acabad339d3
Lena Linhoff <lena.linhoff@tu-dortmund.de>
2019-02-19 09:41:02 +0100

    bayesian significance

--- cta_transient_search/analyse_cube.py
+++ cta_transient_search/analyse_cube.py
@@ -70,6 +70,17 @@ def li_ma_significance(n_on, n_off, alpha=0.2):
     return significance
 
 
+def bayesian_significance(N_on, N_off, alpha):
+    N_all = N_on + N_off
+    gamma = (1 + 2 * N_off) * alpha**(0.5 + N_all) * special.gamma(0.5 + N_all)
+    delta = 2 * (1 + alpha)**N_all * special.gamma(1 + N_all) * special.hyp2f1(0.5 + N_off, 1 + N_all, 1.5 + N_off, -1/alpha)
+    c = np.sqrt(np.pi) / (2 * np.arctan(1/np.sqrt(alpha)))
+    P = gamma / (gamma + c * delta)
+    S_b = np.sqrt(2) * special.erfinv(1 - P)
+
+    return S_b
+
+
 def li_ma_benchmark(cube_raw, n_slices_off, gap):
     alpha = 1 / n_slices_off
     slices = []
@@ -77,6 +88,17 @@ def li_ma_benchmark(cube_raw, n_slices_off, gap):
         n_off = cube_raw[i:i + n_slices_off].sum(axis=0)
         n_on = cube_raw[i + n_slices_off + gap]
         slices.append(li_ma_significance(n_on, n_off, alpha=alpha))
+
+    return slices
+
+
+def bayesian_benchmark(cube_raw, n_slices_off, gap):
+    alpha = 1 / n_slices_off
+    slices = []
+    for i in range(len(cube_raw) - gap - n_slices_off):
+        n_off = cube_raw[i:i + n_slices_off].sum(axis=0)
+        n_on = cube_raw[i + n_slices_off + gap]
+        slices.append(bayesian_significance(n_on, n_off, alpha=alpha))
 
     return slices
 
@@ -148,7 +170,7 @@ def main(
     if background == True:
         cube = cube_raw_table['cube'].data.reshape(-1, bins, bins)
         print('bg', cube.shape)
-        cube_S = li_ma_benchmark(cube, 5, 3)
+        cube_S = bayesian_benchmark(cube, 5, 3)
         pos_trigger_pixel = max_pixel_position(cube_S)
         list_trigger_position.append(pos_trigger_pixel)
         list_cubes_denoised.append(cube_S)
@@ -156,7 +178,7 @@ def main(
     else:
         print('signal')
         for cube in tqdm(cube_raw_table['cube']):
-            cube_S = li_ma_benchmark(cube, 5, 3)
+            cube_S = bayesian_benchmark(cube, 5, 3)
             pos_trigger_pixel = max_pixel_position(cube_S)
 
             list_trigger_position.append(pos_trigger_pixel)
@@ -177,7 +199,7 @@ def main(
         num_slices = cube_raw_table.meta['num_slices']  # in simulate_cube: 3*n_slices
         time_per_slice = cube_raw_table.meta['time_per_slice']
         n_cubes = cube_raw_table.meta['n_cubes']
-        denoised_table.write('{}/n{}_s{}_t{}_bg_lima_denoised.hdf5'.format(
+        denoised_table.write('{}/n{}_s{}_t{}_bg_Sb_denoised.hdf5'.format(
                                                                         output_path,
                                                                         n_cubes,
                                                                         num_slices,
@@ -185,7 +207,7 @@ def main(
                                                                     ), path='data', overwrite=True)
 
         trans_factor_table.meta = denoised_table.meta
-        trans_factor_table.write('{}/n{}_s{}_t{}_bg_lima_trigger.hdf5'.format(
+        trans_factor_table.write('{}/n{}_s{}_t{}_bg_Sb_trigger.hdf5'.format(
                                                                         output_path,
                                                                         n_cubes,
                                                                         num_slices,
@@ -198,7 +220,7 @@ def main(
         transient_template_filename = cube_raw_table.meta['template']
         cu_min = cube_raw_table.meta['min brightness in cu']
         z_trans = cube_raw_table.meta['redshift']
-        denoised_table.write('{}/n{}_s{}_t{}_i{}_cu{}_z{}_lima_denoised.hdf5'.format(
+        denoised_table.write('{}/n{}_s{}_t{}_i{}_cu{}_z{}_Sb_denoised.hdf5'.format(
                                                                         output_path,
                                                                         n_transient,
                                                                         num_slices,
@@ -209,7 +231,7 @@ def main(
                                                                     ), path='data', overwrite=True)
 
         trans_factor_table.meta = denoised_table.meta
-        trans_factor_table.write('{}/n{}_s{}_t{}_i{}_cu{}_z{}_lima_trigger.hdf5'.format(
+        trans_factor_table.write('{}/n{}_s{}_t{}_i{}_cu{}_z{}_Sb_trigger.hdf5'.format(
                                                                         output_path,
                                                                         n_transient,
                                                                         num_slices,
